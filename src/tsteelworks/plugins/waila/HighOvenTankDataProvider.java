4ed1d4e087568da6072bb6bd87783205fe2cdbe5
wisthy <wisthy@users.noreply.github.com>
2014-04-28 11:09:21 +0200

    Update HighOvenTankDataProvider.java
    implementation of this one is strongly based on TConstruct implementation

--- src/tsteelworks/plugins/waila/HighOvenTankDataProvider.java
+++ src/tsteelworks/plugins/waila/HighOvenTankDataProvider.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package tsteelworks.plugins.waila;
 
 import java.util.List;
@@ -10,6 +7,7 @@ import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
 
+// inspired from the one from TConstruct
 public class HighOvenTankDataProvider implements IWailaDataProvider {
 
 	/* (non-Javadoc)
@@ -27,16 +25,39 @@ public class HighOvenTankDataProvider implements IWailaDataProvider {
 	@Override
 	public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
 		// TODO Auto-generated method stub
-		return null;
+		return currenttip;
 	}
-
+	
 	/* (non-Javadoc)
 	 * @see mcp.mobius.waila.api.IWailaBlock#getWailaBody(net.minecraft.item.ItemStack, java.util.List, mcp.mobius.waila.api.IWailaDataAccessor, mcp.mobius.waila.api.IWailaConfigHandler)
 	 */
 	@Override
-	public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {	
+		TileEntity te = accessor.getTileEntity()
+		if (te instanceof DeepTankLogic)
+		{
+			DeepTankLogic dtl = (DeepTankLogic) te;
+			List<FluidStack> fls = dtl.getAllFluids();
+			if(fls != null)
+			{
+				if(fls.size() <= 0)
+				{
+					currenttip.add("§o" + StatCollector.translateToLocal("tconstruct.waila.empty")); // "§o" == Italics
+				}
+				else
+				{
+					for(FluidStack stack : fls)
+					{
+						currenttip.add(WailaRegistrar.fluidNameHelper(st) + " (" + st.amount + "mB)");
+					}
+				}
+			}
+			else
+			{
+				// do what? consider as empty?
+				currenttip.add("§o" + StatCollector.translateToLocal("tconstruct.waila.empty")); // "§o" == Italics
+			}
+		}
+		return currenttip;
 	}
-
 }
