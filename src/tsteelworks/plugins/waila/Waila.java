ac4a97ca10801d96246134688541d226be4a2ca0
wisthy <wisthy@users.noreply.github.com>
2014-04-28 12:38:50 +0200

    Update Waila.java
    cleaned up version

--- src/tsteelworks/plugins/waila/Waila.java
+++ src/tsteelworks/plugins/waila/Waila.java
@@ -1,31 +1,46 @@
 package tsteelworks.plugins.waila;
 
-import cpw.mods.fml.common.event.FMLInterModComms;
-import tsteelworks.TSteelworks;
 import tsteelworks.plugins.ICompatPlugin;
+import cpw.mods.fml.common.event.FMLInterModComms;
 
+//forked from the one from TConstruct
 public class Waila implements ICompatPlugin
 {
+	/*
+	 * (non-Javadoc)
+	 * @see tsteelworks.plugins.ICompatPlugin#getModId()
+	 */
     @Override
     public String getModId ()
     {
         return "Waila";
     }
 
+    /*
+     * (non-Javadoc)
+     * @see tsteelworks.plugins.ICompatPlugin#preInit()
+     */
     @Override
     public void preInit ()
     {
         // Nothing
     }
 
+    /*
+     * (non-Javadoc)
+     * @see tsteelworks.plugins.ICompatPlugin#init()
+     */
     @Override
     public void init ()
     {
-        TSteelworks.logger.info("Waila detected. Registering TSteelwork tank blocks with Waila registry.");
-
+        //TSteelworks.logger.info("Waila detected. Registering TSteelwork tank blocks with Waila registry.");
         FMLInterModComms.sendMessage("Waila", "register", "tsteelworks.plugins.waila.WailaRegistrar.wailaCallback");
-    }
+    }  
 
+    /*
+     * (non-Javadoc)
+     * @see tsteelworks.plugins.ICompatPlugin#postInit()
+     */
     @Override
     public void postInit ()
     {
