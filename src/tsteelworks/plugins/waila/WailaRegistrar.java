41ab158065918bd5ccf27a5a1e2b3ac49347f499
wisthy <wisthy@users.noreply.github.com>
2014-04-28 12:34:43 +0200

    Update WailaRegistrar.java
    cleaned up version

--- src/tsteelworks/plugins/waila/WailaRegistrar.java
+++ src/tsteelworks/plugins/waila/WailaRegistrar.java
@@ -1,30 +1,22 @@
 package tsteelworks.plugins.waila;
 
 import mcp.mobius.waila.api.IWailaRegistrar;
-import net.minecraft.util.StatCollector;
-import net.minecraftforge.fluids.FluidRegistry;
-import net.minecraftforge.fluids.FluidStack;
-import tsteelworks.TSteelworks;
 import tsteelworks.blocks.logic.DeepTankLogic;
-
 
 public class WailaRegistrar
 {
-    public static void wailaCallback (IWailaRegistrar registrar)
-    {
-        TSteelworks.logger.info("[Waila-Compat] Got registrar: " + registrar);
+	public static void wailaCallback (IWailaRegistrar registrar)
+	{
+		//TSteelworks.logger.info("[Waila-Compat] Got registrar: " + registrar);
 
-        // Tanks
-        //registrar.registerBodyProvider(new SearedTankDataProvider(), LavaTankBlock.class);
-        registrar.registerBodyProvided(new HighOvenTankDataProvider(), DeepTankLogic.class);
-        
-        
-    }
+		// Tanks
+		registrar.registerBodyProvider(new HighOvenTankDataProvider(), DeepTankLogic.class);
+	}
 
-    // needed too?
-    public static String fluidNameHelper (FluidStack f)
-    {
-        return StatCollector.translateToLocal(FluidRegistry.getFluidName(f));
-    }
+	// needed too? Let's assume not for the moment.
+	//    public static String fluidNameHelper (FluidStack f)
+	//    {
+	//        return StatCollector.translateToLocal(FluidRegistry.getFluidName(f));
+	//    }
 
 }
