7dea035b1b6ab5f5068aacec1c02a04325256228
Toops <toops@gmx.cm>
2014-04-28 20:52:22 -0500

    Waila Support
    Finished up waila support with newest API, now to do the same for
    TConstruct itself...
    Covers Deep Tanks and the High Oven
    Temporarily provided localized support for TConstruct's fluids using
    waila names in en_US.lang (also to be moved to TConstruct)
    Thanks again, wisthy!

--- src/tsteelworks/plugins/waila/WailaRegistrar.java
+++ src/tsteelworks/plugins/waila/WailaRegistrar.java
@@ -4,16 +4,19 @@ import mcp.mobius.waila.api.IWailaRegistrar;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
+import tsteelworks.TSteelworks;
 import tsteelworks.blocks.logic.DeepTankLogic;
+import tsteelworks.blocks.logic.HighOvenLogic;
 
 public class WailaRegistrar
 {
 	public static void wailaCallback (IWailaRegistrar registrar)
 	{
-		//TSteelworks.logger.info("[Waila-Compat] Got registrar: " + registrar);
+		TSteelworks.logger.info("[Waila-Compat] Got registrar: " + registrar);
 
 		// Tanks
 		registrar.registerBodyProvider(new HighOvenTankDataProvider(), DeepTankLogic.class);
+		registrar.registerBodyProvider(new HighOvenTankDataProvider(), HighOvenLogic.class);
 	}
 
 	// needed too? Let's assume not for the moment.
