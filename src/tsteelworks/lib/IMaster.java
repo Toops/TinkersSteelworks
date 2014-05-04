8a2b18d2def44872d12b35dfd985c43018990878
wisthy <wisthler@gmail.com>
2014-05-04 09:27:40 +0200

    new interface to use in TSMultiServantLogic

---
+++ src/tsteelworks/lib/IMaster.java
@@ -0,0 +1,17 @@
+package tsteelworks.lib;
+
+import tconstruct.library.util.CoordTuple;
+
+public interface IMaster {
+	/**
+	 * 
+	 * @return the coordinate of the master
+	 */
+	public CoordTuple getCoord();
+	
+	/**
+	 * 
+	 * @return true = the master is valid / false = the master is not valid
+	 */
+	public boolean isValid();
+}
