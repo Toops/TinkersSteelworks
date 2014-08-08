11236ae584948d020e06c14b4765e73e80d1aecd
ephys <ephyspotato@gmail.com>
2014-08-08 19:54:16 +0200

    let the port continue

--- src/main/java/tsteelworks/blocks/TSBaseBlock.java
+++ src/main/java/tsteelworks/blocks/TSBaseBlock.java
@@ -1,73 +1,71 @@
 package tsteelworks.blocks;
 
-import java.util.List;
-
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Icon;
-import tsteelworks.common.TSRepo;
-import tsteelworks.lib.TSteelworksRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import tsteelworks.common.TSRepo;
+import tsteelworks.lib.TSteelworksRegistry;
 
-public class TSBaseBlock extends Block
-{
-    public String[] textureNames;
-    public Icon[] icons;
+import java.util.List;
 
-    public TSBaseBlock(int id, Material material, float hardness)
-    {
-        super(id, material);
-        setHardness(hardness);
-        setCreativeTab(TSteelworksRegistry.SteelworksCreativeTab);
-    }
-    
-    public TSBaseBlock(int id, Material material, float hardness, String[] tex)
-    {
-        super(id, material);
-        setHardness(hardness);
-        setCreativeTab(TSteelworksRegistry.SteelworksCreativeTab);
-        textureNames = tex;
-    }
+public class TSBaseBlock extends Block {
+	public String[] textureNames;
+	public IIcon[] icons;
 
-    @Override
-    public int damageDropped (int meta)
-    {
-        return meta;
-    }
+	public TSBaseBlock(Material material, float hardness) {
+		super(material);
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    public Icon getIcon (int side, int meta)
-    {
-        return meta < icons.length ? icons[meta] : icons[0];
-    }
+		setHardness(hardness);
+		setCreativeTab(TSteelworksRegistry.SteelworksCreativeTab);
+	}
 
-    @SideOnly(Side.CLIENT)
-    public int getSideTextureIndex (int side)
-    {
-        if (side == 0)  return 2;
-        if (side == 1) return 0;
-        return 1;
-    }
+	public TSBaseBlock(Material material, float hardness, String[] tex) {
+		super(material);
 
-    @SuppressWarnings ({ "rawtypes", "unchecked" })
-    @Override
-    public void getSubBlocks (int id, CreativeTabs tab, List list)
-    {
-        for (int iter = 0; iter < icons.length; iter++)
-            list.add(new ItemStack(id, 1, iter));
-    }
+		setHardness(hardness);
+		setCreativeTab(TSteelworksRegistry.SteelworksCreativeTab);
+		textureNames = tex;
+	}
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerIcons (IconRegister iconRegister)
-    {
-        icons = new Icon[textureNames.length];
-        for (int i = 0; i < icons.length; ++i)
-            icons[i] = iconRegister.registerIcon(TSRepo.textureDir + textureNames[i]);
-    }
+	@Override
+	public int damageDropped(int meta) {
+		return meta;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int side, int meta) {
+		return meta < icons.length ? icons[meta] : icons[0];
+	}
+
+	@SideOnly(Side.CLIENT)
+	public int getSideTextureIndex(int side) {
+		if (side == 0)
+			return 2;
+		if (side == 1)
+			return 0;
+
+		return 1;
+	}
+
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	@Override
+	public void getSubBlocks(Item block, CreativeTabs tab, List list) {
+		for (int iter = 0; iter < icons.length; iter++)
+			list.add(new ItemStack(block, 1, iter));
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister iconRegister) {
+		icons = new IIcon[textureNames.length];
+		for (int i = 0; i < icons.length; ++i)
+			icons[i] = iconRegister.registerIcon(TSRepo.textureDir + textureNames[i]);
+	}
 }
--- src/main/java/tsteelworks/blocks/TSBaseFluid.java
+++ src/main/java/tsteelworks/blocks/TSBaseFluid.java
@@ -7,74 +7,68 @@ import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 import tsteelworks.common.TSRepo;
 
-public class TSBaseFluid extends BlockFluidClassic
-{
-    String texture;
-    boolean alpha;
-    public Icon stillIcon;
-    public Icon flowIcon;
-    public int renderColor = 16777215;
-    
-    public TSBaseFluid(int id, Fluid fluid, Material material, String texture)
-    {
-        super(id, fluid, material);
-        this.texture = texture;
-    }
+public class TSBaseFluid extends BlockFluidClassic {
+	String texture;
+	boolean alpha;
+	public Icon stillIcon;
+	public Icon flowIcon;
+	public int renderColor = 16777215;
 
-    public TSBaseFluid(int id, Fluid fluid, Material material, String texture, boolean alpha)
-    {
-        this(id, fluid, material, texture);
-        this.alpha = alpha;
-    }
+	public TSBaseFluid(int id, Fluid fluid, Material material, String texture) {
+		super(id, fluid, material);
+		this.texture = texture;
+	}
 
-    @Override
-    public int getRenderBlockPass ()
-    {
-        return alpha ? 1 : 0;
-    }
+	public TSBaseFluid(int id, Fluid fluid, Material material, String texture, boolean alpha) {
+		this(id, fluid, material, texture);
+		this.alpha = alpha;
+	}
 
-//    @Override
-//    public int getBlockColor()
-//    {
-//        return renderColor;
-//    }
-//
-//    @Override
-//    /**
-//     * Returns the color this block should be rendered. Used by leaves.
-//     */
-//    public int getRenderColor(int par1)
-//    {
-//        return renderColor;
-//    }
+	@Override
+	public int getRenderBlockPass() {
+		return alpha ? 1 : 0;
+	}
 
-//    @Override
-    /**
-     * Returns a integer with hex for 0xrrggbb with this color multiplied against the blocks color. Note only called
-     * when first determining what to render.
-     */
-//    public int colorMultiplier(IBlockAccess par1IBlockAccess, int par2, int par3, int par4)
-//    {
-//        return renderColor;
-//    }
-//    
-//    public void setRenderColor(int colorvalue)
-//    {
-//        renderColor = colorvalue;
-//    }
-    
-    @Override
-    public void registerIcons (IconRegister iconRegister)
-    {
-        stillIcon = iconRegister.registerIcon(TSRepo.textureDir + texture);
-        flowIcon = iconRegister.registerIcon(TSRepo.textureDir + texture + "_flow");
-    }
+	//    @Override
+	//    public int getBlockColor()
+	//    {
+	//        return renderColor;
+	//    }
+	//
+	//    @Override
+	//    /**
+	//     * Returns the color this block should be rendered. Used by leaves.
+	//     */
+	//    public int getRenderColor(int par1)
+	//    {
+	//        return renderColor;
+	//    }
 
-    @Override
-    public Icon getIcon (int side, int meta)
-    {
-        if (side == 0 || side == 1)
-            return stillIcon;
-        return flowIcon;
-    }
+	//    @Override
+
+	/**
+	 * Returns a integer with hex for 0xrrggbb with this color multiplied against the blocks color. Note only called
+	 * when first determining what to render.
+	 */
+	//    public int colorMultiplier(IBlockAccess par1IBlockAccess, int par2, int par3, int par4)
+	//    {
+	//        return renderColor;
+	//    }
+	//
+	//    public void setRenderColor(int colorvalue)
+	//    {
+	//        renderColor = colorvalue;
+	//    }
+	@Override
+	public void registerIcons(IconRegister iconRegister) {
+		stillIcon = iconRegister.registerIcon(TSRepo.textureDir + texture);
+		flowIcon = iconRegister.registerIcon(TSRepo.textureDir + texture + "_flow");
+	}
+
+	@Override
+	public Icon getIcon(int side, int meta) {
+		if (side == 0 || side == 1)
+			return stillIcon;
+		return flowIcon;
+	}
 }
