561aa224ee6fe254e0c2b490028a8d7483c50faa
ephys <ephyspotato@gmail.com>
2014-08-16 12:49:13 +0200

    Made the vacuous modifier only draw if the item is held Also it will instant-pickup instead of waiting for the delay to be over

--- src/main/java/tsteelworks/common/modifier/TSActiveOmniMod.java
+++ src/main/java/tsteelworks/common/modifier/TSActiveOmniMod.java
@@ -2,6 +2,7 @@ package tsteelworks.common.modifier;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
@@ -10,12 +11,14 @@ import tconstruct.library.tools.ToolCore;
 import tsteelworks.lib.TSAbilityHelper;
 
 public class TSActiveOmniMod extends ActiveToolMod {
-	// todo: change draw distance depending on tool attributes ?
 	@Override
 	public void updateTool(ToolCore tool, ItemStack stack, World world, Entity entity) {
+		if (!(entity instanceof EntityPlayer) || ((EntityPlayer) entity).getHeldItem() != stack)
+			return;
+
 		NBTTagCompound tags = stack.getTagCompound().getCompoundTag("InfiTool");
 
 		if (tags.hasKey("Vacuous"))
-			TSAbilityHelper.drawItemsToEntity(world, (EntityLivingBase) entity, tags.getInteger("Vacuous"));
+			TSAbilityHelper.drawItemsToEntity(world, (EntityLivingBase) entity, tags.getInteger("Vacuous") + 1);
 	}
 }
