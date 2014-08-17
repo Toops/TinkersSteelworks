ca866d25d08512a23f42eef61df5aeb1c8185634
ephys <ephyspotato@gmail.com>
2014-08-17 16:19:55 +0200

    reimplemented cement

--- src/main/java/tsteelworks/common/blocks/CementFluidBlock.java
+++ src/main/java/tsteelworks/common/blocks/CementFluidBlock.java
@@ -6,11 +6,8 @@ import net.minecraft.world.World;
 import net.minecraftforge.fluids.Fluid;
 import tsteelworks.common.core.TSContent;
 
-/*
- * Todo: implement
- *
- * If I get the idea, which might be wrong, this fluid is supposed to harden when it's in contact with air ?
- */
+import java.util.Random;
+
 public class CementFluidBlock extends TSFluidBlock {
 	public CementFluidBlock(Fluid fluid, Material material, String texture) {
 		super(fluid, material, texture);
@@ -21,16 +18,32 @@ public class CementFluidBlock extends TSFluidBlock {
 		super.onBlockAdded(world, x, y, z);
 	}
 
+	@Override
+	public void updateTick(World world, int x, int y, int z, Random rand) {
+		super.updateTick(world, x, y, z, rand);
+
+		checkForHarden(world, x, y, z);
+	}
+
+	public void checkForHarden(World world, int x, int y, int z) {
+		System.out.println(world.getBlockMetadata(x, y, z) + ", " + quantaPerBlock);
+		if (world.getBlockMetadata(x, y, z) == quantaPerBlock -1 ||
+				validHardenCoords(world, x, y, z - 1) ||
+				validHardenCoords(world, x, y, z + 1) ||
+				validHardenCoords(world, x - 1, y, z) ||
+				validHardenCoords(world, x + 1, y, z) ||
+				validHardenCoords(world, x + 1, y, z + 1) ||
+				validHardenCoords(world, x + 1, y, z - 1) ||
+				validHardenCoords(world, x - 1, y, z + 1) ||
+				validHardenCoords(world, x - 1, y, z - 1)) {
+			world.setBlock(x, y, z, TSContent.cementBlock);
+			world.playSoundEffect(x + 0.5F, y + 0.5F, z + 0.5F, "random.fizz", 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
+		}
+	}
+
 	public boolean validHardenCoords(World world, int x, int y, int z) {
 		Block block = world.getBlock(x, y, z);
 
-		return block.getMaterial() == Material.air || block == TSContent.cementBlock;
-	}
-
-	/**
-	 * Creates fizzing sound and smoke. Used when lava flows over block or mixes with water.
-	 */
-	protected void triggerCementMixEffects(World world, int x, int y, int z) {
-		world.playSoundEffect(x + 0.5F, y + 0.5F, z + 0.5F, "random.fizz", 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
+		return block == TSContent.cementBlock;
 	}
 }
