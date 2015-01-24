c43ca69f25d84c8b7effbededdc88cc43f870e73
ephys <ephyspotato@gmail.com>
2015-01-24 14:53:52 +0100

    Cleanup Minetweaker plugin 1/4

--- src/main/java/toops/tsteelworks/common/plugins/minetweaker3/MinetweakerPlugin.java
+++ src/main/java/toops/tsteelworks/common/plugins/minetweaker3/MinetweakerPlugin.java
@@ -1,11 +1,13 @@
 package toops.tsteelworks.common.plugins.minetweaker3;
 
+import minetweaker.IUndoableAction;
 import minetweaker.MineTweakerAPI;
 import minetweaker.api.item.IItemStack;
 import minetweaker.api.liquid.ILiquidStack;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
+import toops.tsteelworks.api.highoven.IFuelRegistry;
 import toops.tsteelworks.common.plugins.ModCompatPlugin;
 import toops.tsteelworks.common.plugins.minetweaker3.handler.FuelHandler;
 import toops.tsteelworks.common.plugins.minetweaker3.handler.MixAgentHandler;
@@ -38,5 +40,55 @@ public class MinetweakerPlugin extends ModCompatPlugin {
 
 	public static ItemStack parseItem(IItemStack is) {
 		return (ItemStack) is.getInternal();
+	}
+	
+	public static abstract class Remove<Key, Value> implements IUndoableAction {
+		protected final Key key;
+		protected Value removedData;
+
+		public Remove(Key stack) {
+			this.key = stack;
+		}
+
+		@Override
+		public boolean canUndo() {
+			return true;
+		}
+
+		@Override
+		public String describeUndo() {
+			return "Undid " + describe();
+		}
+
+		@Override
+		public Object getOverrideKey() {
+			return null;
+		}
+	}
+	
+	public static abstract class Add<Key, Value> implements IUndoableAction {
+		protected final Key key;
+		protected Value newData;
+		protected Value oldData;
+
+		public Add(Key key, Value newData) {
+			this.key = key;
+			this.newData = newData;
+		}
+
+		@Override
+		public boolean canUndo() {
+			return true;
+		}
+
+		@Override
+		public String describeUndo() {
+			return "Undid " + describe();
+		}
+
+		@Override
+		public Object getOverrideKey() {
+			return null;
+		}
 	}
 }
--- src/main/java/toops/tsteelworks/common/plugins/minetweaker3/handler/FuelHandler.java
+++ src/main/java/toops/tsteelworks/common/plugins/minetweaker3/handler/FuelHandler.java
@@ -1,20 +1,31 @@
 package toops.tsteelworks.common.plugins.minetweaker3.handler;
 
-import minetweaker.IUndoableAction;
 import minetweaker.MineTweakerAPI;
 import minetweaker.api.item.IItemStack;
 import net.minecraft.item.ItemStack;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 import toops.tsteelworks.api.highoven.IFuelRegistry;
+import toops.tsteelworks.api.highoven.IFuelRegistry.IFuelData;
+import toops.tsteelworks.common.plugins.minetweaker3.MinetweakerPlugin;
 
 import static toops.tsteelworks.common.plugins.minetweaker3.MinetweakerPlugin.parseItem;
 
 @ZenClass("mods.tsteelworks.highoven")
 public class FuelHandler {
 	@ZenMethod
-	public static void addFuel(IItemStack fuel, int burnTime, int heatValue) {
-		MineTweakerAPI.apply(new Add(parseItem(fuel), burnTime, heatValue));
+	public static void addFuel(IItemStack fuel, final int burnTime, final int heatValue) {
+		MineTweakerAPI.apply(new Add(parseItem(fuel), new IFuelData() {
+			@Override
+			public int getBurnTime() {
+				return burnTime;
+			}
+
+			@Override
+			public int getHeatRate() {
+				return heatValue;
+			}
+		}));
 	}
 
 	@ZenMethod
@@ -22,88 +33,56 @@ public class FuelHandler {
 		MineTweakerAPI.apply(new Remove(parseItem(fuel)));
 	}
 
-	private static class Add implements IUndoableAction {
-		private final ItemStack fuel;
-		private final int burnTime;
-		private final int heatRate;
-
-		public Add(ItemStack fuel, int burnTime, int heatRate) {
-			this.fuel = fuel;
-			this.burnTime = burnTime;
-			this.heatRate = heatRate;
+	private static class Add extends MinetweakerPlugin.Add<ItemStack, IFuelData> {
+		public Add(ItemStack fuel, IFuelData data) {
+			super(fuel, data);
 		}
 
 		@Override
 		public void apply() {
-			IFuelRegistry.INSTANCE.addFuel(fuel, burnTime, heatRate);
-		}
-
-		@Override
-		public boolean canUndo() {
-			return true;
+			oldData = IFuelRegistry.INSTANCE.addFuel(key, newData.getBurnTime(), newData.getHeatRate());
 		}
 
 		@Override
 		public void undo() {
-			IFuelRegistry.INSTANCE.removeFuel(fuel);
+			if (oldData == null)
+				IFuelRegistry.INSTANCE.removeFuel(key);
+			else
+				IFuelRegistry.INSTANCE.addFuel(key, oldData.getBurnTime(), oldData.getHeatRate());
+			
+			oldData = null;
 		}
 
 		@Override
 		public String describe() {
-			return "Added " + fuel.getDisplayName() + " as valid High Oven fuel.";
-		}
-
-		@Override
-		public String describeUndo() {
-			return "Removed " + fuel.getDisplayName() + " as valid High Oven fuel.";
-		}
-
-		@Override
-		public Object getOverrideKey() {
-			return null;
+			if (oldData == null)
+				return "Added " + key.getDisplayName() + " as valid High Oven fuel.";
+			else
+				return "Replaced " + key.getDisplayName() + " as valid High Oven fuel.";
 		}
 	}
 
-	private static class Remove implements IUndoableAction {
-		private final ItemStack fuel;
-		private int burnTime;
-		private int heatRate;
-
+	private static class Remove extends MinetweakerPlugin.Remove<ItemStack, IFuelData> {
 		public Remove(ItemStack stack) {
-			this.fuel = stack;
+			super(stack);
 		}
 
 		@Override
 		public void apply() {
-			IFuelRegistry.IFuelData data = IFuelRegistry.INSTANCE.removeFuel(fuel);
-
-			burnTime = data.getBurnTime();
-			heatRate = data.getHeatRate();
-		}
-
-		@Override
-		public boolean canUndo() {
-			return true;
+			removedData = IFuelRegistry.INSTANCE.removeFuel(key);
 		}
 
 		@Override
 		public void undo() {
-			IFuelRegistry.INSTANCE.addFuel(fuel, burnTime, heatRate);
-		}
-
-		@Override
-		public String describeUndo() {
-			return "Added " + fuel.getDisplayName() + " as valid High Oven fuel.";
+			if (removedData == null) return;
+			
+			IFuelRegistry.INSTANCE.addFuel(key, removedData.getBurnTime(), removedData.getHeatRate());
+			removedData = null;
 		}
 
 		@Override
 		public String describe() {
-			return "Removed " + fuel.getDisplayName() + " as valid High Oven fuel.";
-		}
-
-		@Override
-		public Object getOverrideKey() {
-			return null;
+			return "Removed " + key.getDisplayName() + " as valid High Oven fuel.";
 		}
 	}
 }
